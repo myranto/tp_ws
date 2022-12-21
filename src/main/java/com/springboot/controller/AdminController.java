54060a2f67e04b531ee6201b8684231dd7c78056
myranto <95678823+myranto@users.noreply.github.com>
2022-12-21 04:57:38 +0300

    Update AdminController.java

--- src/main/java/com/springboot/controller/AdminController.java
+++ src/main/java/com/springboot/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.springboot.controller;
 import com.springboot.FormatToJson.ToJsonData;
 import com.springboot.model.Admin;
 import com.springboot.service.AdminServices;
+import com.springboot.service.TokenService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +14,22 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
 
     private AdminServices as;
+    private TokenService serve;
 
-    public AdminController(AdminServices as) {
+    public AdminController(AdminServices as, TokenService serve) {
         super();
         this.as = as;
+        this.serve = serve;
+    }
+    @GetMapping("/checkToken")
+    public ResponseEntity<ToJsonData> isConnected(@PathVariable("id") int id,@PathVariable("token") String token){
+        try {
+                serve.checkTokens(token,id);
+                return new ResponseEntity<>(new ToJsonData<String>("encore connecter"), HttpStatus.ACCEPTED);
+
+        }catch (Exception e){
+            return  new ResponseEntity<>(new ToJsonData<String>(e.getMessage()), HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @PostMapping("/login")
